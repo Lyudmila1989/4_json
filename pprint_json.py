74c0b1d1c38fe3ef08f88be6ba004cbf6b02ab0a
lyudmila1989 <lyudmila_l_@mail.ru>
2018-01-21 06:34:22 +0700

    Renamed variabels. Fixed filepath input

--- pprint_json.py
+++ pprint_json.py
@@ -1,16 +1,20 @@
 import json
+import sys
 
 
 def load_data(json_filepath):
-    with open(json_filepath) as f:
-        return json.load(f)
+    with open(json_filepath) as json_file:
+        return json.load(json_file)
 
 
 def pretty_print_json(json_content):
-    print(json.dumps(json_content, sort_keys=True, indent=4, ensure_ascii=False))
-    pass
+    print(json.dumps(json_content, sort_keys=True,
+                     indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    json_filepath = input("Input the path to the file: ")
+    try:
+        json_filepath = sys.argv[1]
+    except Exception as Error1:
+        json_filepath = input("Input the path to file ")
     pretty_print_json(load_data(json_filepath))
