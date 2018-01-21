528e046743f9ea936a5b3c242ad4e5b033aad480
lyudmila1989 <lyudmila_l_@mail.ru>
2018-01-21 16:55:47 +0700

    Added I/O exception handling. Improved formatting.

--- pprint_json.py
+++ pprint_json.py
@@ -3,18 +3,27 @@ import sys
 
 
 def load_data(json_filepath):
-    with open(json_filepath) as json_file:
-        return json.load(json_file)
+    try:
+        with open(json_filepath) as json_file:
+            return json.load(json_file)
+    except IOError:
+        print("File not found")
 
 
 def pretty_print_json(json_content):
-    print(json.dumps(json_content, sort_keys=True,
-                     indent=4, ensure_ascii=False))
+    print(
+        json.dumps(
+            json_content,
+            sort_keys=True,
+            indent=4,
+            ensure_ascii=False
+        )
+    )
 
 
 if __name__ == '__main__':
     try:
         json_filepath = sys.argv[1]
-    except IndexError as Error1:
+    except IndexError:
         json_filepath = input("Input the path to file ")
     pretty_print_json(load_data(json_filepath))
